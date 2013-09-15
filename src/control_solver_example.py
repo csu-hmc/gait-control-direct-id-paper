cfc1143c684ce0e6d1cc4daed0d96f3bc6a5f3d5
Jason K. Moore <moorepants@gmail.com>
2013-09-14 21:28:38 -0400

    Added reduced gain matrix solutions.

--- src/control_solver_example.py
+++ src/control_solver_example.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-import pandas
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # external
+import numpy as np
 import matplotlib.pyplot as plt
+import pandas
 
 # local
 from dtk import walk
@@ -12,24 +12,46 @@ from dtk import walk
 obinna = pandas.read_csv('../data/obinna-walking.txt', delimiter='\t',
                          index_col="TimeStamp", na_values='0.000000')
 
+# change the degrees to radians
+for col in obinna.columns:
+    if col.endswith('.Ang'):
+        obinna[col] = np.deg2rad(obinna[col])
+
 start = 500
 stop = 3000
 
 data = walk.WalkingData(obinna.iloc[start:stop].copy())
 
-data.time_derivative(['RKneeFlexion.Ang'], ['RKneeFlexion.Rat'])
+angles = ['RKneeFlexion.Ang',
+          'LKneeFlexion.Ang']
+
+rates = ['RKneeFlexion.Rate',
+         'LKneeFlexion.Rate']
+
+data.time_derivative(angles, rates)
 
 data.grf_landmarks('FP2.ForY', 'FP1.ForY', threshold=28.0)
 
-right_steps = data.split_at('right', num_samples=10)
+right_steps = data.split_at('right', num_samples=50)
 axes = data.plot_steps('FP2.ForY', linestyle='-', marker='o')
 
-sensors = ['RKneeFlexion.Ang', 'RKneeFlexion.Rat']
-controls = ['RKneeFlexion.Mom', 'RHipFlexion.Mom']
+controls = ['RKneeFlexion.Mom',
+            'LKneeFlexion.Mom']
+
+sensors = ['RKneeFlexion.Ang',
+           'RKneeFlexion.Rate',
+           'LKneeFlexion.Ang',
+           'LKneeFlexion.Rate']
+
+
 solver = walk.SimpleControlSolver(right_steps, sensors, controls)
 
+gain_omission_matrix = np.ones((len(controls), len(sensors))).astype(bool)
+gain_omission_matrix[0, 2:] = False
+gain_omission_matrix[1, :2] = False
+
 gains, controls, variance, gain_var, control_var, estimated_controls = \
-    solver.solve()
+    solver.solve(gain_omission_matrix=gain_omission_matrix)
 
 solver.plot_gains(gains, gain_var)
 
