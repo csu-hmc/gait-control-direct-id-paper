9316a0e7f2ada8f4a3c63ea3e5db5a8c6ab99f2b
Jason K. Moore <moorepants@gmail.com>
2015-04-09 14:37:30 -0700

    Added script for plotting the mean gains.

---
+++ src/plot_mean_gains.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+"""This script plots the mean of the identified gains. The gains must be
+precomputed. It currently does not include trials from Subject 9."""
+
+# builtin
+import os
+import argparse
+
+# external
+import matplotlib.pyplot as plt
+
+# local
+import utils
+
+PATHS = utils.config_paths()
+
+
+def main(event, structure):
+
+    file_name_safe_event = '-'.join(event.lower().split(' '))
+    file_name_safe_structure = '-'.join(structure.split(' '))
+
+    plot_dir = utils.mkdir(os.path.join(PATHS['figures_dir'],
+                                        'identification-results',
+                                        file_name_safe_event,
+                                        file_name_safe_structure))
+
+    # Do not include subject 9 in the means because of the odd ankle joint
+    # torques.
+    similar_trials = utils.build_similar_trials_dict(bad_subjects=[9])
+
+    mean_gains_per_speed = {}
+
+    for speed, trial_numbers in similar_trials.items():
+
+        mean_gains, var_gains = utils.mean_gains(
+            trial_numbers, utils.Trial.sensors, utils.Trial.controls,
+            utils.Trial.num_cycle_samples, file_name_safe_event,
+            file_name_safe_structure)
+
+        mean_gains_per_speed[speed] = mean_gains
+
+        fig, axes = utils.plot_joint_isolated_gains(
+            utils.Trial.sensors, utils.Trial.controls, mean_gains,
+            var_gains)
+
+        fig.set_size_inches((14.0, 14.0))
+        fig.savefig(os.path.join(plot_dir, 'mean-gains-' + speed + '.png'),
+                    dpi=300)
+        plt.close(fig)
+
+    fig, axes = plt.subplots(3, 2, sharex=True)
+    linestyles = ['-', '--', ':']
+    speeds = ['0.8', '1.2', '1.6']
+
+    for speed, linestyle in zip(speeds, linestyles):
+        fig, axes = utils.plot_joint_isolated_gains(utils.Trial.sensors,
+                                                    utils.Trial.controls,
+                                                    mean_gains_per_speed[speed],
+                                                    var_gains, axes=axes,
+                                                    show_std=False,
+                                                    linestyle=linestyle)
+    axes[0, 0].legend().set_visible(False)
+    right_labels = ['Right ' + speed + ' [m/s]' for speed in speeds]
+    left_labels = ['Left ' + speed + ' [m/s]' for speed in speeds]
+    leg = axes[1, 0].legend(list(sum(zip(right_labels, left_labels), ())),
+                            loc='best', fancybox=True, fontsize=8)
+    leg.get_frame().set_alpha(0.75)
+
+    fig.savefig(os.path.join(plot_dir, 'mean-gains-vs-speed.png'), dpi=300)
+    plt.close(fig)
+
+if __name__ == "__main__":
+
+    desc = "Identify Controller"
+
+    parser = argparse.ArgumentParser(description=desc)
+
+    msg = ("A valid event name in the data, likely: "
+           "'Longitudinal Perturbation', 'First Normal Walking', "
+           "or 'Second Normal Walking'.")
+    parser.add_argument('-e', '--event', type=str, help=msg,
+                        default='Longitudinal Perturbation')
+
+    msg = ("The desired controller structure: 'join isolated' or 'full'.")
+    parser.add_argument('-s', '--structure', type=str, help=msg,
+                        default='joint isolated')
+
+    args = parser.parse_args()
+
+    main(args.event, args.structure)
