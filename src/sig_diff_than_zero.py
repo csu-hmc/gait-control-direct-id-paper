1d24dd8f93a123e818a075f160dda7b2128bfe00
Jason K. Moore <moorepants@gmail.com>
2015-05-21 16:08:02 -0700

    Compute whether things should be marked.

--- src/sig_diff_than_zero.py
+++ src/sig_diff_than_zero.py
@@ -8,6 +8,7 @@ distribution."""
 import os
 
 # external
+import numpy as np
 from scipy.stats import ttest_1samp
 import pandas as pd
 from statsmodels.api import qqplot
@@ -29,6 +30,7 @@ df = df.loc[:, (df.sum(axis=0) != 0)]
 groups = df.groupby(('nominal_speed', 'event'))
 
 t_test_results = {}
+sig_marks = {}
 
 for speed, event in groups.groups.keys():
 
@@ -57,6 +59,16 @@ for speed, event in groups.groups.keys():
             index.append(col)
             t_vals.append(t_stat)
             p_vals.append(p_val)
+
+    #mark = np.zeros((num_schedules, num_sensors, num_actuators), dtype=bool)
+    mark = np.zeros((20, 6, 12), dtype=bool)
+
+    for gain, p_val in zip(index, p_vals):
+        if p_val < 0.05:
+            i, j, k = [int(n) for n in gain[2:].split('_')]
+            mark[i, j, k] = True
+
+    sig_marks[(speed, event)] = mark
 
     t_test_results[(speed, event)] = pd.DataFrame({'t': t_vals,
                                                    'p': p_vals}, index)
